8c650ba6f09f53e7657ee00fa1874a8bf474b6b1
Ari Rubinstein <ari@agoric.com>
2022-05-11 23:17:39 -0700

    feat: explorer

---
+++ bases/explorer/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cd /workspace
+cp ./src/chains/mainnet/agoric.json /tmp/agoric.json
+cp ./src/chains/testnet/agoric.json /tmp/agoric_test.json
+rm ./src/chains/mainnet/*.json
+rm ./src/chains/testnet/*.json
+mv /tmp/agoric.json ./src/chains/mainnet/agoric.json
+mv /tmp/agoric_test.json ./src/chains/testnet/agoric.json
+
+NETDOMAIN=${NETDOMAIN:-.agoric.net}
+cat ./src/chains/mainnet/agoric.json | sed "s/^.*\"api\": .*/    \"api\": [\"https:\/\/${NETNAME}.api${NETDOMAIN}\"],/g" > /tmp/1.json
+cat /tmp/1.json | sed "s/^.*\"rpc\": .*/    \"rpc\": [\"https:\/\/${NETNAME}.rpc${NETDOMAIN}\"],/g" > ./src/chains/mainnet/agoric.json
+
+cat vue.config.js | sed "s/devServer: {/devServer: {disableHostCheck: true,/g" > /tmp/v.js
+mv /tmp/v.js vue.config.js
+
+yarn && ./node_modules/.bin/vue-cli-service serve --mode=production --port=8080
