1e7413633bd165bffb6088c524b358468cf0f5ef
Dan Connolly <connolly@agoric.com>
2022-08-22 22:08:21 -0500

    chore(psm-load): clean up output; show elapsed time

--- bases/shared/instagoric-server/psm-load.mjs
+++ bases/shared/instagoric-server/psm-load.mjs
@@ -18,11 +18,13 @@ const { AGORIC_D, PSM_TOOL } = process.env;
 const psmTool = PSM_TOOL || 'psm-tool';
 const agd = AGORIC_D || `agd`;
 
-const psmInstance = await $`${psmTool} --contract`;
+const psmInstance = (await $`${psmTool} --contract`.quiet()).stdout.trim();
 // console.debug(psmInstance);
 
 const ensureAccount = async () => {
-  const keys = JSON.parse((await $`${agd} keys list --output json`).stdout);
+  const keys = JSON.parse(
+    (await $`${agd} keys list --output json`.quiet()).stdout,
+  );
   const key = keys.find(({ type }) => type === 'local');
   if (!key) {
     throw Error('no local key in keyring. TODO: agd keys add ...');
@@ -34,7 +36,7 @@ const ensureAccount = async () => {
 const account = await ensureAccount();
 
 const getStatus = async () => {
-  const txt = (await $`${psmTool} --wallet ${account.address}`).stdout;
+  const txt = (await $`${psmTool} --wallet ${account.address}`.quiet()).stdout;
   // undo console.log() abbreviation of keys, JSON.string() of parts
   const json = txt.replace(/(balances|offers):/gm, '"$1":').replace(/'/gm, '');
   return JSON.parse(json);
@@ -46,41 +48,48 @@ const trade = async (
   method,
   qty,
   keyName,
-  { rpc: node, chainId } = networks.xnet,
+  { rpc: node, chainId, blockTime = 6 } = networks.xnet,
 ) => {
   const feePct =
     method === 'wantStable' ? psmGov.WantStableFee : psmGov.GiveStableFee;
-  // why doesn't $`...`.quiet() work?
   const out = $`${psmTool} --boardId ${boardId} --${method} ${qty} --feePct ${
     feePct + 0.001
-  }`;
+  }`.quiet();
   const spendAction = JSON.parse((await out).stdout);
-  console.info(`PSM (${boardId}) trade: ${method} ${qty} (${feePct}% fee)`);
-
-  await $`${agd} --from=${keyName} tx swingset wallet-action --allow-spend ${JSON.stringify(
-    spendAction,
-  )}  --node=${node} --chain-id=${chainId} -y
-  `;
-
-  const blockTime = 6;
   const {
     data: {
       meta: { creationStamp },
     },
   } = spendAction;
   const target = new Date(creationStamp).toISOString();
+  console.info(
+    `${target}: PSM (${boardId}) trade: ${method} ${qty} (${feePct}% fee)`,
+  );
+
+  console.info('tx swingset wallet-action ', { chainId, keyName });
+  const submit = $`${agd} --from=${keyName} tx swingset wallet-action --allow-spend ${JSON.stringify(
+    spendAction,
+  )}  --node=${node} --chain-id=${chainId} -o json -y
+  `;
+  submit.quiet();
+  const { txhash } = JSON.parse((await submit).stdout);
+  console.info({ txhash });
+
+  let elapsed = 0;
   for (;;) {
     const walletStatus = await getStatus();
     const found = walletStatus.offers.find(([dateTime]) => dateTime === target);
+    console.info(JSON.stringify(found));
     const [dateTime, offerStatus] = found || [];
     switch (offerStatus) {
       case 'accept':
-        return found;
+        console.info({ creationStamp, elapsed });
+        return { offer: found, elapsed, txhash };
       case 'rejected':
         throw Error(`${method} offer at ${dateTime} rejected`);
       default:
-        console.info(found);
-        await sleep((blockTime / 2) * 1000); // nyquist: observe at 2x freq
+        await sleep(blockTime * 1000);
+        elapsed += blockTime;
     }
   }
 };
