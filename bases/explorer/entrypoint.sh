6eaea6d281de39ba1d75c318f69891526a536db9
Michael FIG <mfig@agoric.com>
2023-08-28 13:11:29 -0600

    fix: no HMR for explorer

--- bases/explorer/entrypoint.sh
+++ bases/explorer/entrypoint.sh
@@ -8,10 +8,17 @@ mv /tmp/agoric.json ./src/chains/mainnet/agoric.json
 mv /tmp/agoric_test.json ./src/chains/testnet/agoric.json
 
 NETDOMAIN=${NETDOMAIN:-.agoric.net}
-cat ./src/chains/mainnet/agoric.json | jq ".api=[\"https:\/\/${NETNAME}.api${NETDOMAIN}\"] | .rpc=[\"https:\/\/${NETNAME}.rpc${NETDOMAIN}\"]" > /tmp/1.json
+cat ./src/chains/mainnet/agoric.json |
+  jq ".api=[\"https:\/\/${NETNAME}.api${NETDOMAIN}\"] | .rpc=[\"https:\/\/${NETNAME}.rpc${NETDOMAIN}\"]" > /tmp/1.json
 cp /tmp/1.json ./src/chains/mainnet/agoric.json
 
 cat vue.config.js | sed "s/devServer: {/devServer: {allowedHosts: 'all',/g" > /tmp/v.js
 mv /tmp/v.js vue.config.js
 
-yarn && ./node_modules/.bin/vue-cli-service serve --mode=production --port=8080 
+# This is a development service which uses hot reloading.  It caused the
+# governance page not to show.
+# yarn && \
+#  ./node_modules/.bin/vue-cli-service serve --mode=production --port=8080
+
+# Serve static files.
+yarn && yarn build && yarn global add serve && serve -p 8080 -s dist
