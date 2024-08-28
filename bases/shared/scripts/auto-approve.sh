a0c7013a4dea599fca0108d99b48bd34d39100cd
Muneeb Zia <31257991+Muneeb147@users.noreply.github.com>
2024-08-28 20:41:42 +0500

    fixup! feat(script): Auto Approval Script
    Already processed proposals check

--- bases/shared/scripts/auto-approve.sh
+++ bases/shared/scripts/auto-approve.sh
@@ -12,6 +12,19 @@ POLL_INTERVAL=10
 FROM_ACCOUNT=self
 AG_BINARY="$1"
 
+# Initialise Processed Proposals File
+
+PROCESSED_PROPOSALS_FILE="processed_proposals.txt"
+
+# Check if the file exists and reinitialize it on script restart
+if [ -e "$PROCESSED_PROPOSALS_FILE" ]; then
+    > "$PROCESSED_PROPOSALS_FILE"
+    echo "File '$PROCESSED_PROPOSALS_FILE' has been cleared."
+else
+    touch $PROCESSED_PROPOSALS_FILE
+    echo "File '$PROCESSED_PROPOSALS_FILE' created."
+fi
+
 while true; do
     # Query for proposals with status "PROPOSAL_STATUS_VOTING_PERIOD"
     PROPOSALS=$($AG_BINARY query gov proposals --status VotingPeriod --chain-id=$CHAIN_ID --home=$AGORIC_HOME --output json 2> /dev/null)
@@ -23,12 +36,18 @@ while true; do
 
     if [ -n "$PROPOSAL_IDS" ]; then
         for PROPOSAL_ID in $PROPOSAL_IDS; do
+            # Check if the proposal has already been processed to avoid re-vote and processing
+
+            if grep -q $PROPOSAL_ID $PROCESSED_PROPOSALS_FILE; then
+                continue
+            fi
             echo "Voting YES on proposal ID: $PROPOSAL_ID"
 
             # Vote YES on the proposal
             $AG_BINARY tx gov vote $PROPOSAL_ID yes \
                 --from=$FROM_ACCOUNT --chain-id=$CHAIN_ID --keyring-backend=test --home=$AGORIC_HOME --yes > /dev/null 2>&1
 
+            echo $PROPOSAL_ID >> $PROCESSED_PROPOSALS_FILE
             echo "Voted YES on proposal ID: $PROPOSAL_ID"
         done
     else
