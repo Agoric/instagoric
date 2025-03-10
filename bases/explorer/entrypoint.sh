62c187141993c1e367540e60b4b9428098d2609c
usmanmani1122 <usman.sbf@gmail.com>
2025-03-10 18:39:27 +0000

    use rewrite to Cluster IP

--- bases/explorer/entrypoint.sh
+++ bases/explorer/entrypoint.sh
@@ -3,28 +3,53 @@
 set -o errexit -o nounset
 
 COMMIT_HASH="4d2d093560c52e08458f97f451dc1f0690e00094"
-NETDOMAIN=${NETDOMAIN:-".agoric.net"}
 PING_PUB_REPOSITORY_LINK="https://github.com/ping-pub/explorer.git"
 PING_PUB_SOURCE="/workspace"
+SERVER_PORT="8080"
 
-apt-get update
-apt-get install git jq --yes
-corepack enable
+build() {
+  yarn --cwd "$PING_PUB_SOURCE"
+  yarn --cwd "$PING_PUB_SOURCE" build
+}
 
-git clone "$PING_PUB_REPOSITORY_LINK" "$PING_PUB_SOURCE"
-git -C "$PING_PUB_SOURCE" checkout "$COMMIT_HASH"
+copy_logos() {
+  mkdir --parents "$PING_PUB_SOURCE/dist/logos"
+  cp "/entrypoint/agoric.png" "$PING_PUB_SOURCE/dist/logos/agoric.png"
+  cp "/entrypoint/agoric-bld.svg" "$PING_PUB_SOURCE/dist/logos/agoric-bld.svg"
+}
 
-rm --force $PING_PUB_SOURCE/chains/mainnet/*.json $PING_PUB_SOURCE/chains/testnet/*.json
+set_chain_data() {
+  rm --force $PING_PUB_SOURCE/chains/mainnet/*.json $PING_PUB_SOURCE/chains/testnet/*.json
 
-jq ".api[] = \"https://$NETNAME.api$NETDOMAIN\" | .rpc[] = \"https://$NETNAME.rpc$NETDOMAIN\"" \
-  --raw-output \
-  <"/entrypoint/agoric.json" >"$PING_PUB_SOURCE/chains/mainnet/agoric.json"
+  jq '.api[] = "/api" | .rpc[] = "/rpc"' \
+    --raw-output \
+    <"/entrypoint/agoric.json" >"$PING_PUB_SOURCE/chains/mainnet/agoric.json"
+}
 
-yarn --cwd "$PING_PUB_SOURCE"
-yarn --cwd "$PING_PUB_SOURCE" build
+setup() {
+  apt-get update
+  apt-get install git jq --yes
+  corepack enable
+}
 
-mkdir --parents "$PING_PUB_SOURCE/dist/logos"
-cp "/entrypoint/agoric.png" "$PING_PUB_SOURCE/dist/logos/agoric.png"
-cp "/entrypoint/agoric-bld.svg" "$PING_PUB_SOURCE/dist/logos/agoric-bld.svg"
+setup_repository() {
+  git clone "$PING_PUB_REPOSITORY_LINK" "$PING_PUB_SOURCE"
+  git -C "$PING_PUB_SOURCE" checkout "$COMMIT_HASH"
+  patch --directory "$PING_PUB_SOURCE" --input "/entrypoint/config.patch" --strip "1"
+  cat "$PING_PUB_SOURCE/vite.config.ts"
+}
 
-yarn --cwd "$PING_PUB_SOURCE" preview --host --port 8080
+start_server() {
+  yarn \
+    --cwd "$PING_PUB_SOURCE" \
+    preview \
+    --host \
+    --port "$SERVER_PORT"
+}
+
+setup
+setup_repository
+set_chain_data
+build
+copy_logos
+start_server
