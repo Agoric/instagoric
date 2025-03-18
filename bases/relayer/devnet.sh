6734b23d219d4f5ac4c91de7e5f8a6a00cadace3
usmanmani1122 <usman.sbf@gmail.com>
2025-03-18 14:24:28 +0000

    feat: multiple relayer paths

---
+++ bases/relayer/devnet.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+
+set -o errexit -o errtrace
+
+CONFIG_FILE_PATH="$RELAYER_HOME/devnet.json"
+CHAIN_ADDRESS_PREFIX="agoric"
+CHAIN_GAS_DENOM="ubld"
+CHAIN_ID="agoricdev-25"
+CHAIN_NAME="devnet"
+CHAIN_RPC="https://devnet.rpc.agoric.net:443"
+SKELETON_OBJECT=""
+
+add_chain() {
+    relayer chains add "$CHAIN_NAME" --file "$CONFIG_FILE_PATH" --home "$RELAYER_HOME"
+}
+
+create_skeleton() {
+    SKELETON_OBJECT="$(
+        jq '
+    {
+      "type": "cosmos",
+      "value": {
+        "account-prefix": "",
+        "chain-id": "",
+        "coin-type": 564,
+        "debug": true,
+        "gas-adjustment": 1.2,
+        "gas-prices": "",
+        "key": "default",
+        "keyring-backend": "test",
+        "output-format": "json",
+        "rpc-addr": "",
+        "sign-mode": "direct",
+        "timeout": "20s"
+      }
+    }' --null-input
+    )"
+}
+
+main() {
+    create_skeleton
+    write_config_file
+    add_chain
+}
+
+write_config_file() {
+    echo "$SKELETON_OBJECT" |
+        jq --arg prefix "$CHAIN_ADDRESS_PREFIX" \
+            --arg chain_id "$CHAIN_ID" \
+            --arg gas_prices "0.025$CHAIN_GAS_DENOM" \
+            --arg rpc "$CHAIN_RPC" '
+      .value["account-prefix"] = $prefix |
+      .value["chain-id"] = $chain_id |
+      .value["gas-prices"] = $gas_prices |
+      .value["rpc-addr"] = $rpc
+    ' >"$CONFIG_FILE_PATH"
+}
+
+main
