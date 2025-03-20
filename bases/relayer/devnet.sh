a0040882f6e3aee7b53f905cc206c293603b3264
usmanmani1122 <usman.sbf@gmail.com>
2025-03-20 07:45:34 +0000

    use network config for data

--- bases/relayer/devnet.sh
+++ bases/relayer/devnet.sh
@@ -3,9 +3,12 @@
 set -o errexit -o errtrace -o nounset
 
 DIRECTORY_PATH="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+NETWORK_CONFIG_URL="https://devnet.agoric.net/network-config"
 SCRIPT_NAME="$(basename "$0")"
 
-CHAIN_ID="agoricdev-25" \
+network_config="$(curl --fail --location --silent "$NETWORK_CONFIG_URL")"
+
+CHAIN_ID="$(echo "$network_config" | jq --raw-output '.chainName')" \
     CHAIN_NAME="${SCRIPT_NAME%.sh}" \
-    CHAIN_RPC="https://devnet.rpc.agoric.net:443" \
+    CHAIN_RPC="$(echo "$network_config" | jq --raw-output '.rpcAddrs[0]')" \
     /bin/bash "$DIRECTORY_PATH/add-agoric-chain.sh"
